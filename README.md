fe0921bf192389efa84fa02214ff830d74555e5e
Jakub Biesiada <biesiadajakub@icloud.com>
2019-05-07 15:16:08 +0200

    Add config files

---
+++ README.md
@@ -0,0 +1,4 @@
+# [react-night](https://github.com/jb1905/react-night)
+
+## License
+This project is licensed under the MIT License © 2019-present Jakub Biesiada
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = env => {
+  const libraryName = 'react-night';
+
+  let outputFile, mode, devtool;
+
+  if (env === 'build') {
+    mode = 'production';
+    outputFile = `${libraryName}.min.js`;
+    devtool = false;
+  } else {
+    mode = 'development';
+    outputFile = `${libraryName}.js`;
+    devtool = 'source-map';
+  }
+
+  return {
+    mode,
+    entry: `${__dirname}/src/index.tsx`,
+    externals: ['night.js', 'react'],
+    devtool,
+    output: {
+      path: `${__dirname}/lib`,
+      filename: outputFile,
+      library: 'ReactNight',
+      libraryTarget: 'commonjs2'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/,
+          resolve: {
+            extensions: ['.tsx']
+          }
+        }
+      ]
+    }
+  };
+};

2b71cd7952c6e9e9a159b7e0683c62044eaedfbf
Jakub Biesiada <biesiadajakub@icloud.com>
2019-06-22 02:01:18 +0200

    Update config & devDependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   rules: {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'react/display-name': 'off',
     'no-unused-vars': 'off'
   },
   settings: {

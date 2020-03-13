b3a13f7bde2f4acc6bf0e9bb087bd37c05f3a6be
Jakub Biesiada <biesiadajakub@icloud.com>
2020-03-13 14:03:07 +0100

    Update types

--- lib/withNight.d.ts
+++ lib/withNight.d.ts
@@ -1,2 +1,2 @@
 import React from 'react';
-export declare const withNight: (WrappedComponent: React.ComponentClass<{}, any>) => (props: any) => JSX.Element;
+export declare const withNight: (WrappedComponent: React.ComponentType<any>) => (props: any) => JSX.Element;
--- src/withNight.tsx
+++ src/withNight.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 import { useNight } from './useNight';
 
-export const withNight = (WrappedComponent: React.ComponentClass) => (
+export const withNight = (WrappedComponent: React.ComponentType<any>) => (
   props: React.ComponentProps<any>
 ) => <WrappedComponent {...props} {...useNight()} />;

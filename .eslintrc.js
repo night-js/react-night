c03384ad487c9bcdc6428200d6859528b739055a
Jakub Biesiada <biesiadajakub@icloud.com>
2020-06-28 23:26:42 +0200

    chore: update

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,30 +3,30 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:prettier/recommended',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
   ],
   plugins: ['prettier', 'react-hooks'],
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'module',
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
   rules: {
     'react/prop-types': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/display-name': 'off',
-    'no-unused-vars': 'off'
+    'no-unused-vars': 'off',
   },
   settings: {
     react: {
-      version: 'detect'
-    }
-  }
+      version: 'detect',
+    },
+  },
 };

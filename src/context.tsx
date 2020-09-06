42c1aa527e1666b05749c25bc2133fa107056504
Jakub Biesiada <biesiadajakub@icloud.com>
2020-09-06 14:47:16 +0200

    chore: update

--- src/context.tsx
+++ src/context.tsx
@@ -1,47 +1,10 @@
 import React from 'react';
 import Night from 'night.js';
 
-type Settings = {
-  readonly auto: boolean;
-  readonly brightness: boolean;
-  readonly brightnessHighClass: string;
-  readonly brightnessLowClass: string;
-  readonly checkBreakpoint: boolean;
-  readonly darkClass?: string;
-  readonly lightClass?: string;
-  readonly mode: 'smart' | 'color-scheme';
-  readonly permissionDelay: number;
-  readonly soundTimeout: number;
-  readonly soundUrl?: string;
-  readonly storage: boolean;
-  readonly storageClear: boolean;
-};
-
-type Callbacks = {
-  onAccess: () => void;
-  onAccessDenied: () => void;
-  onBrightness: () => void;
-  onBrightnessNotSupported: () => void;
-  onDark: () => void;
-  onInit: () => void;
-  onToggle: () => void;
-  onLight: () => void;
-  onSmartSwitch: () => void;
-  onSoundPlay: () => void;
-  onSoundPause: () => void;
-  onStorageClear: () => void;
-};
-
-type Config = {
-  readonly settings: Settings;
-  readonly callbacks: Callbacks;
-};
-
-// TODO add types
 export const NightContext = React.createContext<any>(null);
 
 export const NightProvider: React.FC<{
-  readonly config?: Partial<Config>;
+  readonly config?: any;
 }> = ({ children, config }) => {
   const night = new Night(config);
 
--- src/useNight.ts
+++ src/useNight.ts
@@ -2,39 +2,8 @@ import { useContext } from 'react';
 
 import { NightContext } from './context';
 
-type Output = {
-  readonly toggle: () => void;
-  readonly light: () => void;
-  readonly dark: () => void;
-  readonly smartSwitch: () => void;
-  readonly setCoords: (latiutde: number, longitude: number) => void;
-  readonly setTimes: (sunrise: number, sunset: number) => void;
-  readonly reset: () => void;
-  readonly destroy: () => void;
-  readonly night: object;
-};
-
-export const useNight = (): Output => {
+export const useNight = () => {
   const night = useContext(NightContext);
 
-  const toggle = night.toggle;
-  const light = night.light;
-  const dark = night.dark;
-  const smartSwitch = night.smartSwitch;
-  const setCoords = night.setCoords;
-  const setTimes = night.setTimes;
-  const reset = night.reset;
-  const destroy = night.destroy;
-
-  return {
-    toggle,
-    light,
-    dark,
-    smartSwitch,
-    setCoords,
-    setTimes,
-    reset,
-    destroy,
-    ...night,
-  };
+  return night;
 };
